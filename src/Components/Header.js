3b29d3e2f66fd3fc407d21bdd0ca7d73e5a6b049
katchen1 <katherinechen139@gmail.com>
2024-05-19 13:06:52 -0700

    make video fullscreen in background

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,12 +8,21 @@ class Header extends Component {
   render() {
     if (!this.props.data) return null;
 
+    const project = this.props.data.project;
+    const github = this.props.data.github;
+    const description = this.props.data.description;
+
     return (
       <header id="home">
+        {/* <ParticlesBg type="circle" bg={true} /> */}
 
         <nav className="navbar" id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
             Show navigation
+          </a>
+
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
           </a>
 
           <ul id="nav" style={{ backgroundColor: 'black', fontFamily: 'Marion' }}>
@@ -89,14 +98,47 @@ class Header extends Component {
           </ul>
         </nav>
 
+        <div className="row banner">
+          <div className="video-container">
+            <video autoPlay controls className="background-video">
+              <source src={David} type="video/mp4" />
+            </video>
+          </div>
+          <div className="banner-text">
+            <Fade bottom>
+              <h1 className="responsive-headline">Mercedes-Benz AI Palette</h1>
+            </Fade>
+            <Fade bottom duration={1200}>
+              <h3>{description}.</h3>
+            </Fade>
+            <hr />
+            <Fade bottom duration={2000}>
+              <ul className="social">
+                <a href={project} className="button btn project-btn">
+                  <i className="fa fa-book"></i>Project
+                </a>
+                <a href={github} className="button btn github-btn">
+                  <i className="fa fa-github"></i>Github
+                </a>
+              </ul>
+            </Fade>
+          </div>
+        </div>
 
-      <div className="banner">
-      <video className="fullscreen-video" controls autoPlay>
-        <source src={David} type="video/mp4" />
-      </video>
-      </div>
-      <h1>Mercedes-Benz AI Palette</h1>
-      <h3>Conversational AI Personas</h3>
+        <p className="scrolldown">
+          <Link
+            className="link"
+            activeClass="active"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={750}
+          >
+            <i className="icon-down-circle"></i>
+          </Link>
+        </p>
+
       </header>
       
 
